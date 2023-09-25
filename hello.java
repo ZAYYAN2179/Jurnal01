82b6f5c63f616a07e9fc41e1e1897fca26e889ba
unknown <aatzaayyan49@gmail.com>
2023-09-25 15:21:56 +0700

    first commit

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class hello {
+    public static void main(String[] args) {
+    System.out.println("Nama : Muh Zayyan Al Thaaf Nur");
+    System.out.println("NIM : 607062330072");
+    System.out.println("Kelas : D3IF-47-03");
+    }
+
+}
